b0c2f25daf9002bb10a13f3b446a7171fe68caa4
sasvercelsas <sshail102@outlook.com>
2026-02-06 21:35:52 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import Home from "./pages/Home";
-
-function App() {
-  return <Home />;
-}
-
-export default App;
 // src/App.tsx
 import React from 'react';
-import { MakeupFunnel } from './components/MakeupFunnel'; // Import your new component
+import { MakeupFunnel } from './components/MakeupFunnel';
 
 function App() {
   return (
     <div className="App">
-      {/* This renders your entire funnel flow */}
+      {/* The funnel is the entry point of the site */}
       <MakeupFunnel />
     </div>
   );
