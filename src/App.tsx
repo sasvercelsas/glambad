604468d1720d2dce4fcdb2694946eb1649d688ce
sasvercelsas <sshail102@outlook.com>
2026-02-06 21:41:14 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-// src/App.tsx
 import React from 'react';
-import { MakeupFunnel } from './components/ui/Makeupquiz';
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      {/* The funnel is the entry point of the site */}
-      <MakeupFunnel />
+      <Home />
     </div>
   );
 }
